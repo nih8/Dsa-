f89636ee00e51da0b3681d3c8f87dfc21419ecd8
HimanshiArya17 <b24ee1025@iitj.ac.in>
2025-11-05 22:51:49 +0530

    Update to_insert_a_word.cpp

--- DSA project/to_insert_a_word.cpp
+++ DSA project/to_insert_a_word.cpp
@@ -6,6 +6,59 @@
 #include <fstream>
 #include <ctype.h>
 #include <sstream>
+
 using namespace std;
+struct TrieNode{
+    TrieeNode* next[26]; 
+    bool isend; 
+    int cnt; 
+
+      TrieNode() {
+        for(int i=0;i<26;i++){
+            next[i]=NULL;
+        }
+        isend=false;
+        cnt=0;
+    }
+};
+
+class Trie{
+public:
+    TrieNode* root;
+
+    Trie(){
+        root=new TrieNode();
+    }
+
+    char tolower(char ch){
+        if(ch>='A' && ch<='Z'){
+            ch-='A'+'a';
+        }
+        return ch;
+    }
+
+    bool isletter(char ch){
+        return(c>='a' && c<='z');
+    }
+
+    void insert(string word){
+        TrieNode* curr=root;
+        int n=word.size();
+        for(int i=0;i<n;i++) {
+            char c=tolower(word[i]);
+            if(!isletter(c)){
+                continue;
+            }
+            int ind=c-'a';
+            if(curr->next[ind]==NULL){
+                curr->next[ind]=new TrieNode();
+            }
+            curr=curr->next[ind];
+        }
+      
+        curr->isend= true;
+        curr->cntt++;
+    }
+
 
 
