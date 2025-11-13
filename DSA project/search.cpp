0aa2a8d87291a15932701f3a70fde20fc5f0af07
Sumana-Nimmagadda <b24cs1049@iitj.ac.in>
2025-11-13 11:08:12 +0530

    Update search.cpp

--- DSA project/search.cpp
+++ DSA project/search.cpp
@@ -17,3 +17,48 @@ struct TrieNode {
     }
 };
 
+class Trie {
+public:
+    TrieNode* root;
+
+    Trie() {
+        root = new TrieNode();
+    }
+
+    char tolower(char c) {
+        if (c >= 'A' && c <= 'Z') return c - 'A' + 'a';
+        return c;
+    }
+
+    bool isletter(char c) {
+        return (c >= 'a' && c <= 'z');
+    }
+
+    // Insert one word
+    void insert(string word) {
+        TrieNode* curr = root;
+        for (char c : word) {
+            c = tolower(c);
+            if (!isletter(c)) continue;
+            int idx = c - 'a';
+            if (!curr->next[idx])
+                curr->next[idx] = new TrieNode();
+            curr = curr->next[idx];
+        }
+        curr->isEnd = true;
+        curr->cnt++;
+    }
+
+    // Search exact word
+    bool search(string word) {
+        TrieNode* curr = root; // pointer starts searching from root node, moves from letter to letter
+        for (char c : word) {
+            c = tolower(c); // converts to lower case
+            if (!isletter(c)) continue;
+            int idx = c - 'a';
+            if (!curr->next[idx])
+                return false;
+            curr = curr->next[idx]; // next letter of the word
+        }
+        return curr->isEnd; // all letters of the word are checked
+    }
