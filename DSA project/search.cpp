40b1fdd7d8df087f6723698abdee1c111c5baa98
Sumana-Nimmagadda <b24cs1049@iitj.ac.in>
2025-11-14 21:10:08 +0530

    Update search.cpp

--- DSA project/search.cpp
+++ DSA project/search.cpp
@@ -19,37 +19,38 @@ struct TrieNode {
 
 class Trie {
 public:
-    TrieNode* root;
+    TrieNode* root; // pointer named root that points to a TrieNode(starting point of trie)
 
     Trie() {
-        root = new TrieNode();
+        root = new TrieNode(); // Creates a new TrieNode on the heap and sets root to point to it
     }
 
-    char tolower(char c) {
+    char tolower(char c) /*converts an uppercase letter to lowercase */{
         if (c >= 'A' && c <= 'Z') return c - 'A' + 'a';
         return c;
     }
 
-    bool isletter(char c) {
+    bool isletter(char c)/* checks if a character is a lowercase letter */{
         return (c >= 'a' && c <= 'z');
     }
 
     // Insert one word
     void insert(string word) {
-        TrieNode* curr = root;
+        TrieNode* curr = root; // Creates a pointer curr that starts at the root of the trie.
         for (char c : word) {
             c = tolower(c);
             if (!isletter(c)) continue;
             int idx = c - 'a';
             if (!curr->next[idx])
                 curr->next[idx] = new TrieNode();
-            curr = curr->next[idx];
+            curr = curr->next[idx]; // Move curr to this child node to process the next character
         }
-        curr->isEnd = true;
+        curr->isEnd = true; // mark this node as the end of a word
         curr->cnt++;
     }
 
-    // Search exact word
+    // Search exact word: true if the word exists, false otherwise.
+
     bool search(string word) {
         TrieNode* curr = root; // pointer starts searching from root node, moves from letter to letter
         for (char c : word) {
@@ -63,19 +64,19 @@ public:
         return curr->isEnd; // all letters of the word are checked
     }
 
-    // Autocomplete helper
+    // Autocomplete helper: used to print all words in the trie that start with a given prefix
 
-    void suggestions(TrieNode* node, string prefix) {
-        if (!node) return;
-        if (node->isEnd)
+    void suggestions(TrieNode* node, string prefix) /* node: the current TrieNode; prefix: the string formed so far(root to node)*/{
+        if (!node) return; // stop the recursion, prevents accessing a non-existent node
+        if (node->isEnd) //end of a word
             cout << prefix << " (" << node->cnt << ")\n";
         for (int i = 0; i < 26; i++) {
             if (node->next[i])
-                suggestions(node->next[i], prefix + char('a' + i));
+                suggestions(node->next[i], prefix + char('a' + i)); // Calls suggestions recursively on that child node.
         }
     }
 
-    // Autocomplete main
+    // Autocomplete main: it prints suggestions directly
 
     void autocomplete(string prefix) {
         TrieNode* curr = root;
@@ -83,7 +84,7 @@ public:
             c = tolower(c);
             if (!isletter(c)) continue;
             int idx = c - 'a';
-            if (!curr->next[idx]) {
+            if (!curr->next[idx]) /*no word in the trie starts with this prefix.*/{
                 cout << "No suggestions found for \"" << prefix << "\"\n"; // if no matching list is found by the end of the new word entered
                 return;
             }
